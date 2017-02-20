1a8bf76f2654fddece08d76e8aa5b8b86b65538f
KatieAJenkins <kind2karma@gmail.com>
2017-02-19 18:13:11 -0700

    added testing criteria

--- tests/tests.js
+++ tests/tests.js
@@ -1,4 +1,17 @@
 'use strict';
 
+// Two Sum: Write a function that takes an array of integers and a target integer. It should find two different items in the array that, when added together, give the target value. The indices of these items should then be returned in an array like so: [index1, index2]. // input: [1, 2, 3, 4, 5, 6, 7], 13 // output: [5,6]
+
 var expect = require('chai').expect;
 var code = require('../findtarget.js');
+
+describe('findtarget' , function() {
+//input should contain 2 arguments
+//argument1 should be an array
+//argument2 should be a number
+//input should contain only numbers
+//output should be an array
+//output should add up to argument2
+
+
+});
